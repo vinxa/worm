c1b131f92b9a282aa8c8a7071b0f6db0ef8c56bf
Gabby <vinxa17@gmail.com>
2025-05-22 13:16:11 +0800

    refactoredd to use first column as identifier rather than sequential grouping. add some extra game types, support for late players

--- server_scripts/convert_game_files.py
+++ server_scripts/convert_game_files.py
@@ -35,7 +35,8 @@ def readFile(filepath):
     }
 
     sections = {key: [] for key in known_headers}
-    current_section = None
+    code_to_section = {}
+    raw_records = []  # to preserve file order for 4 and 5 sections
 
     with open(filepath, newline='', encoding='utf-16-le') as csvfile:
         reader = csv.reader(csvfile, delimiter='\t')
@@ -44,21 +45,34 @@ def readFile(filepath):
                 continue
 
             if row[0].startswith(";"):
-                try:
-                    section = row[0].split("/", 1)[1].split()[0]
-                    current_section = section if section in known_headers else None
-                except IndexError:
-                    current_section = None
-            elif current_section:
-                data = row[1:]  # Skip index field
-                headers = known_headers[current_section]
-                row_dict = {key: val for key, val in zip(headers, data)}
-                sections[current_section].append(row_dict)
+                semicode, rest = row[0][1:].split("/", 1)
+                section = rest.split()[0]
+                if section in known_headers:
+                    code_to_section[semicode] = section
+            else:
+                code = row[0]
+                section = code_to_section.get(code)
+                if not section:
+                    continue
+            
+                # Special case cos the column is variable width
+                if section == "event":
+                    row_dict = {
+                        "time":   row[1],
+                        "type":   row[2],
+                        "varies": " ".join(row[3:]).strip()
+                    }
+                else:
+                    data    = row[1:]  # Skip the code
+                    headers = known_headers[section]
+                    row_dict = {key: val for key, val in zip(headers, data)}
 
-    return sections
+                sections[section].append(row_dict)
+                raw_records.append((section, row_dict))
+
+    return sections, raw_records
 
 if __name__ == "__main__":
-    print("RUINNING")
     if len(sys.argv) != 2:
         print("Usage: python convert_game_files.py <input_filepath>")
         sys.exit(1)
@@ -66,7 +80,7 @@ if __name__ == "__main__":
     filepath = sys.argv[1]
     output_dir = Path(__file__).parent.parent / "data/games"
     output_dir.mkdir(parents=True, exist_ok=True)  # Check it exists
-    parsed_data = readFile(filepath)
+    parsed_data, raw_records = readFile(filepath)
 
     # Set basic game info
     output["gameDuration"] = int(int(parsed_data["mission"][0]["duration"]) / 1000 )
@@ -118,78 +132,120 @@ if __name__ == "__main__":
             }
             bases[player_data["id"]] = base
 
+    # Check for late logins in startup sequence
+    for state in parsed_data["player-state"]:
+        if state["entity"].startswith("#") and state["entity"] not in output["players"].keys():
+            print(state)
+
     # Set events
-    for i, event in enumerate(parsed_data["score"]):
-        if "delta" not in event and event["entity"] != "0101":
+    for i, (section, event) in enumerate(raw_records):
+        if section != "event":
             continue
 
-        event_data = {"time": int(event["time"])/1000}
+        # Check for late logins
+        #if event["entity"].startswith("#") and event["entity"] not in output["players"].keys():
 
-        match event["entity"]:
-            case "0201":  # miss
-                continue
+        t = int(event["time"])/1000
 
-            case "0206" | "0208":  # deacs
-                event_data.update({"entity": event["old"], "target": event["new"], "type":"tag", "delta": int(parsed_data["score"][i-2]["delta"])})
-
-                # Append event for tagged player
-                output["events"].append({"time":event_data["time"], "entity":event_data["target"], "target":event_data["entity"], "type":"tagged", "delta":int(parsed_data["score"][i-1]["delta"])})
-                
-            case "0203":  # shoot base
-                event_data.update({
-                    "entity": event["old"],
-                    "delta": int(parsed_data["score"][i-1]["delta"]),
-                    "type": "base hit",
-                    "target": bases[event["new"]]["team"]})
-                
-            case "0204":  # destroy base
-                event_data.update({
-                    "entity": event["old"],
-                    "delta": int(parsed_data["score"][i-1]["delta"]),
-                    "type": "base destroy",
-                    "target": bases[event["new"]]["team"]})
-                
-            case "0B01" | "0B02":  # deny (technically denies or denied, both are same here.)
-                event_data.update({
-                    "entity": event["old"],
-                    "target": event["new"],
-                    "type":"deny",
-                    "delta": int(parsed_data["score"][i-1]["delta"])
-                })
-
-                # Append event for denied player. Not score change but good to keep track
-                output["events"].append({"time":event_data["time"], "entity":event_data["target"], "target":event_data["entity"], "type":"denied", "delta": 0})
-
-            case "0600":  # penalty
-                event_data.update({
-                    "entity": event["old"],
-                    "delta": int(parsed_data["score"][i-1]["delta"]),
-                    "type": "penalty",
-                    "target": ""})
-                
-            case "0500":  # reload
-                continue
-            
+        match event["type"]:
             case "0101":  # game end
                 print("Game end")
                 for player in output["players"].values():
                     output["events"].append({
-                        "time": event_data["time"],
+                        "time": t,
                         "entity":player["id"], 
                         "target":"", 
                         "type":"game end", 
                         "delta": 0})
                 continue
 
-            case "0207":  # stun
+            case "0100":  # game start
+                print("Game start")
+                for player in output["players"].values():
+                    output["events"].append({
+                        "time": t,
+                        "entity":player["id"], 
+                        "target":"", 
+                        "type":"game start", 
+                        "delta": 0})
                 continue
+
+            case "0201" | "0500" | "0207" | "0902":  
+                # miss, reload, stun, achievements
+                continue
+
+            case "0206" | "0208":  # deacs
+                tagger = raw_records[i-2][1]
+                tagged = raw_records[i-1][1]
+                output["events"].append({
+                    "time":t,
+                    "entity":tagger["entity"],
+                    "target":tagged["entity"],
+                    "type":"tag",
+                    "delta":int(tagger["delta"])
+                    })
+                # Append event for tagged player
+                output["events"].append({
+                    "time":t,
+                    "entity":tagged["entity"],
+                    "target":tagger["entity"],
+                    "type":"tagged",
+                    "delta":int(tagged["delta"])
+                    })
+                
+            case "0203":  # shoot base
+                score_event = raw_records[i-1][1]
+                base_id = event["varies"].split()[-1].strip()
+                output["events"].append({
+                    "time": t,
+                    "entity": score_event["entity"],
+                    "delta": int(score_event["delta"]),
+                    "type": "base hit",
+                    "target": bases[base_id]["team"]
+                })
+                
+            case "0204":  # destroy base
+                score_event = raw_records[i-1][1]
+                base_id = event["varies"].split()[-1].strip()
+                output["events"].append({
+                    "time": t,
+                    "entity": score_event["entity"],
+                    "delta": int(score_event["delta"]),
+                    "type": "base destroy",
+                    "target": bases[base_id]["team"]
+                })
+                
+            case "0B01" | "0B02":  # deny (technically denies or denied, both are same here.)
+                score_event = raw_records[i-1][1]
+                denied = event["varies"].split()[-1].strip()
+                output["events"].append({
+                    "time": t,
+                    "entity": score_event["entity"],
+                    "target": denied,
+                    "type": "deny",
+                    "delta": int(score_event["delta"])
+                })
+
+                # Append event for denied player. Not score change but good to keep track
+                output["events"].append({
+                    "time": t,
+                    "entity": denied,
+                    "target": score_event["entity"],
+                    "type":"denied",
+                    "delta": 0})
+
+            case "0600":  # penalty
+                output["events"].append({
+                    "time": t,
+                    "entity": event["varies"].split()[0].strip(),
+                    "delta": int(raw_records[i-1][1]["delta"]),
+                    "type": "penalty",
+                    "target": ""})
 
             case _:
                 if event["entity"] not in output["players"].keys():
                     print(f"Unknown event: {event}")
                 continue
-
-        output["events"].append(event_data)
 
     print(f"Writing to {output_path}...")
     with open(output_path, "w", encoding="utf-8") as jsonfile:
